5beeff80ea94fda3bbe0b7f35fa2ae95347221fa
Felix Lutze <felix.lutze@outlook.de>
2020-09-10 14:47:31 +0200

    Added conditional statements and more variables

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-titles = [
+posts = [
     {
-        'name': 'First title'
+        'author': 'Felix L.',
+        'title': 'Blog Post 1',
+        'content': 'Example content.',
+        'date': 'September 9, 2020'
     },
     {
-        'name': 'Second title'
+        'author': 'Lutze F.',
+        'title': 'Blog Post 2',
+        'content': 'More example content.',
+        'date': 'September 10, 2020'
     }
 
 ]
@@ -15,12 +21,12 @@ titles = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', titles=titles)
+    return render_template('home.html', posts=posts, pageTitle="CustomHome")
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', pageTitle="CostomAbout")
 
 
 if __name__ == '__main__':
