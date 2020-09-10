81d1e45a0c85cc313b198b9cf0af0cc87fcccdf4
Felix Lutze <felix.lutze@outlook.de>
2020-09-10 14:28:01 +0200

    Passing variable to HTML template

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+titles = [
+    {
+        'name': 'First title'
+    },
+    {
+        'name': 'Second title'
+    }
+
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', titles=titles)
 
 
 @app.route('/about')
